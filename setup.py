cd0f747a200c812b9d917f9f9a811b4f0a619e95
Jason Hite <jasonmhite@gmail.com>
2016-03-31 13:48:30 -0400

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="jasons_plot_style",
+    version="0.0.1",
+    author="Jason M. Hite",
+    license="BSD",
+    packages="jasons_plot_style",
+    install_requires=["matplotlib", "seaborn"],
+)
